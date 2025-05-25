48feb1380c6b77e14d6fce82045f3713716e011a
Yevhen Suprunenko <gollandolion@gmail.com>
2025-05-25 22:50:57 +0200

    chore: add activity section

--- js/index.js
+++ js/index.js
@@ -11,3 +11,24 @@ anchors.forEach((anchor) => {
     }
   });
 });
+
+const roles = [
+  "Natural explorer",
+  "patriot",
+  "astronomer",
+  "Astronomical researcher",
+  "poet",
+  "Russian language reformers",
+  "academician",
+];
+
+function shuffleArray(array) {
+  const arr = [...array];
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
+
+console.log(roles);
